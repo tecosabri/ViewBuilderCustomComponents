666a11ce35ccfcb5f13ab13a35b62b38014d436a
Ismael Sabri <hisme14@gmail.com>
2022-10-11 19:40:56 +0200

    Initial Commit

---
+++ ViewBuilderPrueba/ViewBuilderPruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderPruebaApp.swift
+//  ViewBuilderPrueba
+//
+//  Created by Ismael Sabri Pérez on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderPruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
